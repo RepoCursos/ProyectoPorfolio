f65f87e87f9ce5b5245edaf385135e2f22516bba
Paulo Ubunto <paulofernandez1981@hotmail.com>
2025-03-16 01:31:44 -0300

    Modificacion archivos JS y CSS

--- js/script.js
+++ js/script.js
@@ -1,24 +1,34 @@
-//Funcion que me aplica al estilo a la opcion seleccionada en el menu y quita la previamente seleccionada 
-function seleccionar(link){
+// Función que aplica el estilo a la opción seleccionada en el menú
+// y quita la previamente seleccionada
+function seleccionar(link) {
+    // Seleccionamos todos los enlaces
     var opciones = document.querySelectorAll('#links a');
-    opciones[0].className="";
-    opciones[1].className="";
-    opciones[2].className="";
-    opciones[3].className="";
-    opciones[4].className="";
-    link.className = "seleccionado";
+    
+    // Quitamos la clase "seleccionado" a todos los enlaces
+    opciones.forEach(opcion => opcion.classList.remove('seleccionado'));
+    
+    // Agregamos la clase "seleccionado" al enlace clicado
+    link.classList.add('seleccionado');
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion en el modo responsivo
+    // Hacemos desaparecer el menú cuando se selecciona una opción en el modo responsivo
     var x = document.getElementById("nav");
-        x.className = "";
+    x.classList.remove("responsive");
 }
 
-//Funcion que muestra el menu responsivo 
-function responsiveMenu(){
+
+// Función que muestra el menú responsivo
+function responsiveMenu() {
     var x = document.getElementById("nav");
-    if (x.className === "") {
-        x.className = "responsive";
+    var iconoNav = document.getElementById("icono-nav");
+    
+    // Toggle para el menú
+    x.classList.toggle("responsive");
+    
+    // Cambiar el atributo aria-expanded para accesibilidad
+    if (x.classList.contains("responsive")) {
+        iconoNav.setAttribute("aria-expanded", "true");
     } else {
-        x.className = "";
+        iconoNav.setAttribute("aria-expanded", "false");
     }
 }
+
